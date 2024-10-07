433099942bdd05332b5f9b26c06bd7386853ec97
Fra-Vaz92 <francovazza92@gmail.com>
2024-10-07 20:44:24 +0100

    added login view, main view and signup view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,26 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItme("user"));
+  const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
+  const [user, setUser] = useState(storedUser? storedUser : null);
+
 
   useEffect(() => {
-    fetch("https://movie-app-47zy.onrender.com/movies")
-    .then((response) => response.json());
+    if (!token)
+      return;
+
+    fetch("https://movie-app-47zy.onrender.com/movies", {
+    headers: { Authorization: `Bearer ${token}` },
   })
+    .then((response) => response.json())
     .then((data) => {
       const moviesFromApi = data.map((movie) => {
        return{ _id: movie._id,
@@ -25,10 +35,20 @@ export const MainView = () => {
     });
     setMovies(moviesFromApi);
 })
-.catch((error) => {
-  console.error("Error fetching movies:", error);
-  setError(error.message);
-  },  []);
+  },  [token]);
+  
+  if (!user) {
+    return ( 
+    <>
+    <LoginView onLoggedIn={(user, token) => {
+    setUser(user);
+    setToken(token);
+    }} />
+    or 
+    <SignupView />
+    </>
+  );
+  }
 
   if (selectedMovie) {
     return (
@@ -51,6 +71,9 @@ export const MainView = () => {
           }}
         />
       ))}
+    </div>,
+    <div>
+    <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
     </div>
   );
 }
