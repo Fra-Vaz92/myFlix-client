a667ea04a64875180d380162e504d89719a0023c
Fra-Vaz92 <francovazza92@gmail.com>
2024-10-08 21:31:02 +0100

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import React from "react";
 import PropTypes from "prop-types";
 
@@ -37,37 +36,3 @@ MovieCard.propTypes = {
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
 };
-=======
-import PropTypes from "prop-types";
-export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-      <div
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        {movie.Title}
-      </div>
-    );
-  };
-
-  MovieCard.propTypes = {
-    movie: PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      Title: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired,
-      Genre: PropTypes.shape({
-        Name: PropTypes.string.isRequired
-      }).isRequired,
-      Director: PropTypes.shape({
-        Name: PropTypes.string.isRequired,
-        Bio: PropTypes.string,
-        Birth: PropTypes.string,
-        Death: PropTypes.string
-      }).isRequired,
-      ImagePath: PropTypes.string.isRequired,
-      Featured: PropTypes.bool.isRequired
-    }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
-  };
->>>>>>> Stashed changes
