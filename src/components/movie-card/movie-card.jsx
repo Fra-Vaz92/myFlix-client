1ffacac57ace7524bb44f8ff743bba87edb26937
Fra-Vaz92 <francovazza92@gmail.com>
2024-10-13 19:03:10 +0100

    created a new branch and Bootsrap elements

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-      <div
-      onClick={() => onMovieClick(movie)}
-      className="movie-card" 
-    >
-      <img
-        src={movie.ImagePath}
-        alt={movie.Title}
-        className="w-100" 
-      />
-      <h3>{movie.Title}</h3>
-    </div>
+      <Card className="h-100">
+        <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Description}</Card.Text>
+        <Button onClick={() => onMovieClick(movie)} variant="link">
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
